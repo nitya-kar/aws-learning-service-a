1f90b25fb14dc2cbaea6ec9ce328bde53aa136b6
Nitya <nitya.cse.hit@gmail.com>
2025-10-18 12:35:12 +0530

    Update UserController.java

--- src/main/java/com/opt/demo/aws_learning_service_a/controller/UserController.java
+++ src/main/java/com/opt/demo/aws_learning_service_a/controller/UserController.java
@@ -12,6 +12,12 @@ public class UserController {
 
     @GetMapping("/health")
     public ResponseEntity<String> getUserServiceHealth(){
-        return ResponseEntity.ok("System monitoring shows the user service is functioning correctly, with no anomalies detected.");
+        String inetAddress = null;
+        try {
+            inetAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            return ResponseEntity.internalServerError().body("Unable to determine IP address");
+        }
+        return ResponseEntity.ok("System monitoring shows the user service is functioning correctly, with no anomalies detected on the server "+ inetAddress);
     }
 }
