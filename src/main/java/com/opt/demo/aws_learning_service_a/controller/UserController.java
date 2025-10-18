d97da1ab6e58467c7b27e9a28b8ca6b6b86212d7
Nitya <nitya.cse.hit@gmail.com>
2025-10-18 12:42:50 +0530

    Update UserController.java

--- src/main/java/com/opt/demo/aws_learning_service_a/controller/UserController.java
+++ src/main/java/com/opt/demo/aws_learning_service_a/controller/UserController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
